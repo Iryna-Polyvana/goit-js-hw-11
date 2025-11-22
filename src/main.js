770175de4af5677f529543cad850b7332e1ccecc
Iryna Polyvana <irynapolyvana@gmail.com>
2025-11-22 19:04:49 +0100

    js gallery

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
+import { getImagesByQuery } from "./js/pixabay-api";
 
 
-
+// console.log(getImagesByQuery('cat'));
 
 // У файлі main.js напиши всю логіку роботи додатка.Виклики нотифікацій iziToast, усі перевірки на довжину масиву в отриманій відповіді робимо саме в цьому файлі.Імпортуй в нього функції із файлів pixabay - api.js та render - functions.js та викликай їх у відповідний момент.
