d0952907764f2ca1de0b88c643ec12a0705fe971
Iryna Polyvana <irynapolyvana@gmail.com>
2025-11-22 21:08:16 +0100

    add image search logic and styles

--- src/main.js
+++ src/main.js
@@ -1,6 +1,45 @@
 import { getImagesByQuery } from "./js/pixabay-api";
+import { createGallery, clearGallery, showLoader, hideLoader } from "./js/render-functions";
+import iziToast from "izitoast";
 
 
-// console.log(getImagesByQuery('cat'));
+const searchForm = document.querySelector('.form');
 
-// У файлі main.js напиши всю логіку роботи додатка.Виклики нотифікацій iziToast, усі перевірки на довжину масиву в отриманій відповіді робимо саме в цьому файлі.Імпортуй в нього функції із файлів pixabay - api.js та render - functions.js та викликай їх у відповідний момент.
+const onSubmitForm = event => {
+    event.preventDefault();
+
+    const { target: form } = event;
+    const query = form.elements['search-text'].value.trim();
+    if (query === '') {
+        iziToast.warning({
+            message: 'Please enter a search query.',
+            position: "topRight",
+        })
+        return;
+    }
+    clearGallery();
+    showLoader();
+
+    getImagesByQuery(query)
+        .finally(() => {
+            hideLoader();
+            searchForm.reset();
+        })
+        .then(response => {
+            if (response.data.hits.length === 0) {
+                iziToast.info({
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                    position: "topRight",
+                });
+                return;
+            }
+            createGallery(response.data.hits);
+        })
+        .catch(err => {
+            iziToast.error({
+                message: err.message || 'Something went wrong',
+            })
+        });
+};
+
+searchForm.addEventListener('submit', onSubmitForm);
